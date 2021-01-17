65f7bc2c4ac2f85f93b5200acdd8463566015149
k99sharma <kalash.strt@gmail.com>
2021-01-17 13:55:11 +0530

    Index Page is added
    HTML Page is made using bootstrap v5.0
    Index Router is created to render homepage using ejs view engine.
    Responsive navbar is created.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('home.ejs');
+  res.send(200);
 });
+
+
 
 module.exports = router;
